57f7b52f9571a0523dd3335584666a0c9cf3f9ac
Orel Naranjo <or.naranjo@duocuc.cl>
2023-05-11 21:06:02 -0400

    Agregado formulario de contacto final y validacion

--- js/custom.js
+++ js/custom.js
@@ -15,6 +15,54 @@ function getFecha(){
 /* ********* JQuery Personalizado ********* */
 $(document).ready(function () {
 
+  //Validaciones Formulario Contacto
+
+  $('#contact-form').submit(function(event) {
+    event.preventDefault(); // Evita que se envíe el formulario
+
+    // Validar campos
+    var nombre = $('#nombre').val().trim();
+    var correo = $('#correo').val().trim();
+    var telefono = $('#telefono').val().trim();
+    var mensaje = $('#mensaje').val().trim();
+
+    // Validar nombre
+    if (nombre === '') {
+      $('#nombre').addClass('is-invalid');
+    } else {
+      $('#nombre').removeClass('is-invalid');
+    }
+
+    // Validar correo electrónico
+    var correoPattern = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    if (!correoPattern.test(correo)) {
+      $('#correo').addClass('is-invalid');
+    } else {
+      $('#correo').removeClass('is-invalid');
+    }
+
+    // Validar número de teléfono
+    var telefonoPattern = /^\d{9}$/;
+    if (!telefonoPattern.test(telefono)) {
+      $('#telefono').addClass('is-invalid');
+    } else {
+      $('#telefono').removeClass('is-invalid');
+    }
+
+    // Validar mensaje
+    if (mensaje === '') {
+      $('#mensaje').addClass('is-invalid');
+    } else {
+      $('#mensaje').removeClass('is-invalid');
+    }
+
+    // Si todos los campos son válidos, se puede enviar el formulario
+    if (nombre !== '' && correoPattern.test(correo) && telefonoPattern.test(telefono) && mensaje !== '') {
+      // Carga la ventana modal
+      $('#success-modal').modal('show');
+    }
+  });
+
   //Desvanecer Spinner de Carga
   $(".backcover").fadeOut(1000);
 
